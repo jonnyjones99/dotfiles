d3808e84d9189da86dcb31dcb1c84ca27f445b01
jonnyjones99 <72031997+jonnyjones99@users.noreply.github.com>
2023-08-18 14:28:26 +0100

    bug(nvim): updated treesitter and loads of error now! :/

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -9,9 +9,9 @@ vim.opt.list = true
 -- vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup({
-	show_current_context = true,
-	show_current_context_start = true,
-	show_end_of_line = true,
-	use_treesitter = true,
-	use_treesitter_scope = true,
+	-- show_current_context = true,
+	-- show_current_context_start = true,
+	-- show_end_of_line = true,
+	-- use_treesitter = true,
+	-- use_treesitter_scope = true,
 })
