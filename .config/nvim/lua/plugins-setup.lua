a334c6b0db66e4900d9610eb1ce4fd49ec7f4c8d
jonnyjones99 <72031997+jonnyjones99@users.noreply.github.com>
2023-12-04 14:56:00 +0000

    FEAT: add laravel/statamic_CMS support

--- .config/nvim/lua/plugins-setup.lua
+++ .config/nvim/lua/plugins-setup.lua
@@ -272,10 +272,34 @@ require("lazy").setup({
 		end,
 	},
 
-	--emmet
-	-- {
-	-- 	"mattn/emmet-vim",
-	-- },
+	--laravel help
+	{
+		"adalessa/laravel.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+			"tpope/vim-dotenv",
+			"MunifTanjim/nui.nvim",
+		},
+		cmd = { "Sail", "Artisan", "Composer", "Npm", "Yarn", "Laravel" },
+		keys = {
+			{ "<leader>la", ":Laravel artisan<cr>" },
+			{ "<leader>lr", ":Laravel routes<cr>" },
+			{ "<leader>lm", ":Laravel related<cr>" },
+			{
+				"<leader>lt",
+				function()
+					require("laravel.tinker").send_to_tinker()
+				end,
+				mode = "v",
+				desc = "Laravel Application Routes",
+			},
+		},
+		event = { "VeryLazy" },
+		config = function()
+			require("laravel").setup()
+			-- require("telescope").load_extension("laravel")
+		end,
+	},
 
 	--astro support
 	{
