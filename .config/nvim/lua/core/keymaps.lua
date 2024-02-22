1b81ede47a3d63136496b6ba0bb83d0c4f0ef77c
jonnyjones99 <72031997+jonnyjones99@users.noreply.github.com>
2024-02-22 15:58:42 +0000

    FEAT(nvim): add telescope
    TODO: remove fzf-lua? Not sure if it's needed

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -36,6 +36,13 @@ keymap.set("n", "<C-t>", ":NvimTreeToggle<cr>", { silent = true })
 -- \\ in this case just means \, but you use a \ as an escape char in lua
 keymap.set("n", "\\", ":FzfLua files<cr>", { silent = true })
 
+-- Telescope
+local builtin = require("telescope.builtin")
+keymap.set("n", "<leader>ff", builtin.find_files, {})
+keymap.set("n", "<leader>fg", builtin.live_grep, {})
+keymap.set("n", "<leader>fb", builtin.buffers, {})
+keymap.set("n", "<leader>fh", builtin.help_tags, {})
+
 --bufferline
 keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", {})
 keymap.set("n", "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", {})
