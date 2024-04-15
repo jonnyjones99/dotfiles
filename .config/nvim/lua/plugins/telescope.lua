dd002589faba45ac74677ee724701f6d33cddab4
Jonathan Jones <jonathanjones@Jonathans-MacBook-Pro.local>
2024-04-15 10:05:45 +0100

    CHORE: fix telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,20 +1,8 @@
-local ok, telescope = pcall(require, "telescope")
+-- local ok, telescope = pcall(require, "telescope")
 
-if not ok then
-	vim.notify("telescope couldn't be loaded")
-	return
-end
+-- if not ok then
+-- 	vim.notify("telescope couldn't be loaded")
+-- 	return
+-- end
 
-telescope.setup({
-	pickers = {
-		colorscheme = { enable_preview = true },
-		find_files = {
-			find_command = { "fd", "--type", "f", "--hidden", "--ignore-file", ".gitignore", "--strip-cwd-prefix" },
-		},
-		live_grep = {
-			layout_strategy = "vertical",
-		},
-		buffers = { ignore_current_buffer = true },
-	},
-	extensions_list = { "themes", "terms" },
-})
+require("telescope").setup({})
